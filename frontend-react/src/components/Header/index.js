720a8c3e5294ba4e060b803f45fe78907c2d7469
Adriel dos Santos <adrieldossantos7@gmail.com>
2020-10-12 15:35:19 -0300

    update

--- frontend-react/src/components/Header/index.js
+++ frontend-react/src/components/Header/index.js
@@ -1,41 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom'
+import eua from '../../assets/icons/eua.png'
+import {SignOutAlt} from '@styled-icons/fa-solid/SignOutAlt'
+import {Palette} from '@styled-icons/boxicons-solid/Palette'
+import {Star} from '@styled-icons/boxicons-solid/Star'
 
 
 export default function Header() {
+
+  const [name, setName] = useState('')
+  const [theme, setTheme] = useState('')
+
+
+  useEffect(()=>{
+    if(localStorage.getItem('user_MyEnglish')){
+      let user = JSON.parse(localStorage.getItem('user_MyEnglish'))
+      let theme = localStorage.getItem('theme_MyEnglish')
+
+      if(user){
+        setName(user.name)
+        alert('true')
+      }else {
+        setName('Login')
+        alert('false')
+      }
+
+      if(theme === 'whiteMode'){
+        setTheme('Tema - black')
+      }else if(theme === 'darkMode') {
+        setTheme('Tema - white')
+      }
+      
+ }
+  },[])
+
+  function signOut(){
+    localStorage.removeItem('user_MyEnglish')
+    window.location.reload()
+  }
+
  return (
    <div>
 <nav class="navbar navbar-expand-lg navbar-light bg-dark text-light">
-  <Link to="/"><a class="navbar-brand text-light" href="#">Myenglish</a></Link>
+  <Link to="/"><a class="navbar-brand text-light ml-5 d-flex" href="#">MyEnglish <img src={eua} className="ml-2"/></a></Link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse text-light" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active text-light">
-        <a class="nav-link text-light" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item text-light">
-        <a class="nav-link text-light" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown text-light">
-        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu text-light" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
+
+     
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-   
+    <form class="form-inline my-2 my-lg-0 mr-5">
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {name}
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item d-flex align-items-center" href="#">Conquistas <Star size={15} className="ml-1" color="grey"/></a>
+          <a class="dropdown-item " href="#">{theme} <Palette size={15} className="ml-1" color="grey"/></a>
+          <a class="dropdown-item d-flex align-items-center" href="#" onClick={()=>{signOut()}}>Sair <SignOutAlt size={15} className="ml-1" color="grey"/></a>
+        </div>
+    </div>
     </form>
   </div>
 </nav>
--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'jquery'
 import 'jquery-ui-dist/jquery-ui'
+import 'jquery/dist/jquery.min.js'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
